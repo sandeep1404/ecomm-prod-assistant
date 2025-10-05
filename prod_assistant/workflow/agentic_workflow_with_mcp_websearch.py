7f87b2d1ae4f7d693f942b57ca55b2b2bb3c1046
sandeep1404 <sandeep.kodam99@gmail.com>
2025-10-05 22:50:16 +0900

    port updated

--- prod_assistant/workflow/agentic_workflow_with_mcp_websearch.py
+++ prod_assistant/workflow/agentic_workflow_with_mcp_websearch.py
@@ -34,7 +34,7 @@ class AgenticRAG:
             {
             "hybrid_search": {
             "transport": "streamable_http",
-            "url": "http://localhost:8000/mcp"
+            "url": "http://localhost:8001/mcp"
         }
         }
         )
